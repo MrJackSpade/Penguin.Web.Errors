965e6173223b2c82c85360e9086cfae435740cb3
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-10 13:31:24 -0700

    Autocommit
    Autocommit

--- ExceptionRoute.cs
+++ ExceptionRoute.cs
@@ -3,8 +3,11 @@
     internal class ExceptionRoute
     {
         public string Action { get; set; }
+
         public string Area { get; set; }
+
         public string Controller { get; set; }
+
         public string Template { get; set; }
     }
 }
